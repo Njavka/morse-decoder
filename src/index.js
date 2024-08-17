ec3b416f63e8d8ac80bf92207894e907a18382f4
Navka <navka@list.ru>
2024-08-17 21:59:44 +0300

    feat: created function to convert numbers to Morse code and decode it to words

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const STRING = expr.match(/.{1,10}/g);
+    let decodedString = "";
+    for (let part of STRING) {
+      if (part === "**********") {
+        // Если часть равна '**********', заменяем на пробел
+        decodedString += " ";
+      } else {
+        // Удаляем начальные нули
+        let zerosDelete = part.replace(/^0+/, "");
+        // Заменяем '10' на '.' и '11' на '-'
+        let morseString = zerosDelete.replace(/10/g, ".").replace(/11/g, "-");
+        // Декодируем строку Морзе
+        decodedString += MORSE_TABLE[morseString];
+      }
+    }
+
+    return decodedString;
 }
 
 module.exports = {
